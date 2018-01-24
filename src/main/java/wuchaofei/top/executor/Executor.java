79b6c4262143c14a02e5d5f34a0596a506f59e05
jspdba <jspdba@163.com>
2018-01-24 10:52:26 +0800

    重新打包

--- src/main/java/wuchaofei/top/executor/Executor.java
+++ src/main/java/wuchaofei/top/executor/Executor.java
@@ -1,9 +1,6 @@
 package wuchaofei.top.executor;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /**
  * 命令调用者
@@ -12,6 +9,7 @@ import java.io.InputStreamReader;
 
 abstract class Executor {
     public static String ADB_PATH  = "d:/zhongliang/adb/";
+    private OutputStream os;
 
     protected void callCmd(String ...cmd) throws Exception{
         Process process = null;
@@ -33,18 +31,40 @@ abstract class Executor {
     }
     protected String callCmd(String command) throws Exception{
         System.out.println(command);
-        Process process = Runtime.getRuntime().exec(new String[]{"cmd.exe", "/C", command},null, new File(ADB_PATH));
+        /*if(os==null){
+            Process process = Runtime.getRuntime().exec(new String[]{"cmd.exe", "/C", "su"}, null, new File(ADB_PATH));
+            os=process.getOutputStream();
+        }
+        os.write(command.getBytes());
+        os.flush();*/
+        final Process process = Runtime.getRuntime().exec(new String[]{"cmd.exe", "/C", command},null, new File(ADB_PATH));
         BufferedReader stdoutReader = new BufferedReader(new InputStreamReader(process.getInputStream(),"GBK"));
-        BufferedReader stderrReader = new BufferedReader(new InputStreamReader(process.getErrorStream(),"GBK"));
         String line, result = "";
 
         while ((line = stdoutReader.readLine()) != null) {
             System.out.println(line);
             result += line;
         }
-        while ((line = stderrReader.readLine()) != null) {
-            System.out.println(line);
-        }
+
+        /*为"错误输出流"单独开一个线程读取之,否则会造成标准输出流的阻塞*/
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                BufferedReader stderrReader = null;
+                try {
+                    String line;
+                    stderrReader = new BufferedReader(new InputStreamReader(process.getErrorStream(),"GBK"));
+                    while ((line = stderrReader.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
         process.waitFor();
         process.destroy();
         return result;
